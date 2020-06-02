b16f2c4f40fc9239f496c5f87ae6d265a765a88f
AndrewCS149 <andrewcs149@gmail.com>
2020-06-02 15:29:25 -0700

    Create image filter for drop-down menu.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'Use Strict';
 
 let allAnimals = [];
+const menu = $('#animal-menu');
+const photos = $('#photo-template').html();
 
 function Animals(obj) {
   this.url = obj.image_url;
@@ -13,8 +15,7 @@ function Animals(obj) {
 
 Animals.prototype.render = function () {
   // grab element
-  const photos = $('#photo-template').html();
-  const $newSection = $(`<section>${photos}</section>`);
+  const $newSection = $(`<section data-render="dynamic">${photos}</section>`);
 
   $newSection.find('h2').text(this.title);
   $newSection.find('p').text(this.description);
@@ -23,10 +24,37 @@ Animals.prototype.render = function () {
   $('main').append($newSection);
 }
 
+// function to display the animal drop-down menu
+const dropDown = () => {
 
+  let keywords = [];
 
+  allAnimals.forEach((animal, i) => {
+    const $newOption = $('<option></option>');
+    if (!keywords.includes(`${allAnimals[i].keyword}`)) {
 
+      $newOption.attr('value', `${allAnimals[i].keyword}`);
+      $newOption.text(`${allAnimals[i].keyword}`);
+      keywords.push(`${allAnimals[i].keyword}`);
+      menu.append($newOption);
+    }
+  })
+}
 
+// event to render the images of the title selected from 
+// the drop down menu
+menu.on('change', function () {
+
+  $('section[data-render="dynamic"]').remove();
+
+  allAnimals.forEach(animal => {
+    if (animal.keyword === this.value) {
+      animal.render();
+    }
+  })
+})
+
+// grab animal data from page-1.json
 $.ajax('data/page-1.json', {
     method: 'GET',
     dataType: 'JSON'
@@ -35,4 +63,5 @@ $.ajax('data/page-1.json', {
     data.forEach(animal => {
       new Animals(animal).render();
     });
+    dropDown();
   })
